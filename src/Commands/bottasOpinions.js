d7f0c432d72ad62467356d056a5a73c37c93685e
MarcusKyung <Kyungmj@gmail.com>
2023-06-25 18:21:26 -0700

    add getBottasOpinion() file

---
+++ src/Commands/bottasOpinions.js
@@ -0,0 +1,11 @@
+// function getBottasOpinion() {
+//   if (randomNumber === 1) {
+//     return "My car number is 77. When asked why I chose it I said \"Its pretty simple I liked seven, so I thought seven-seven would be good. And actually seven was already taken by Kimi [Räikkönen]. So then I just thought double seven would be good, so go for it.\"";
+//   } else if (randomNumber === 2) {
+//     return "I was born in Finland but currently reside in Monaco."
+//   }
+// }
+
+module.exports = {
+  getBottasOpinion,
+};
